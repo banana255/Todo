669ff03c4389fdf4d33fd28fe47915e0ad0009da
BigBanan <bigbananas@qq.com>
2017-01-15 00:49:34 +0800

    todo v0.1.0

--- todo.js
+++ todo.js
@@ -3,12 +3,15 @@ var log = function() {
     console.log.apply(console, arguments)
 }
 
+var e = (sel) => document.querySelector(sel)
+
+
 var bindBlur = function() {
-    var todoContainer = document.querySelector('#id-div-container')
+    var todoContainer = e('#id-div-container')
     todoContainer.addEventListener('blur', function(){
         log('container blur', event, event.target)
         var target = event.target
-        var index = indexOfElement(target)
+        var index = indexOfElement(target.parentElement)
         todoList[index].task = target.innerHTML
         saveTodos()
         target.setAttribute('contenteditable', 'false')
@@ -17,15 +20,16 @@ var bindBlur = function() {
 
 var bindAddButton = function() {
     // 给 add button 绑定添加 todo 事件
-    var addButton = document.querySelector('#id-button-add')
+    var addButton = e('#id-button-add')
     addButton.addEventListener('click', function(){
         // 获得 input.value
-        var todoInput = document.querySelector('#id-input-todo')
+        var todoInput = e('#id-input-todo')
         var task = todoInput.value
         // 生成 todo 对象
         var todo = {
             'task': task,
-            'time': currentTime()
+            'time': currentTime(),
+            'finish': false,
         }
         todoList.push(todo)
         saveTodos()
@@ -33,8 +37,21 @@ var bindAddButton = function() {
     })
 }
 
-var bindKeyEnter = function() {
-    var todoContainer = document.querySelector('#id-div-container')
+var bindAddkeyUp = function() {
+    var input = e('#id-input-todo')
+    input.addEventListener('keyup', function(event){
+        log('keyup')
+        if (input.value.length > 0) {
+            e('#id-button-add').removeAttribute('disabled')
+        } else {
+            e('#id-button-add').setAttribute('disabled', '')
+        }
+    })
+
+}
+
+var bindUpdateKeyEnter = function() {
+    var todoContainer = e('#id-div-container')
     todoContainer.addEventListener('keydown', function(event){
         log('container keydown', event, event.target)
         var target = event.target
@@ -45,7 +62,7 @@ var bindKeyEnter = function() {
             // 阻止默认行为的发生, 也就是不插入回车
             event.preventDefault()
             // 更新 todo
-            var index = indexOfElement(target)
+            var index = indexOfElement(target.parentElement)
             log('update index',  index)
             // 把元素在 todoList 中更新
             todoList[index].task = target.innerHTML
@@ -57,20 +74,21 @@ var bindKeyEnter = function() {
 }
 
 var bindDoneDeleteEdit = function() {
-    var todoContainer = document.querySelector('#id-div-container')
+    var todoContainer = e('#id-div-container')
     todoContainer.addEventListener('click', function(event){
-        log('container click', event, event.target)
         var target = event.target
+        var todoDiv = target.parentElement
+        var index = indexOfElement(todoDiv)
+        log('index is', index)
         if(target.classList.contains('todo-done')) {
             log('done')
             // 给 todo div 开关一个状态 class
-            var todoDiv = target.parentElement
             toggleClass(todoDiv, 'done')
+            todoList[index].finish = todoDiv.classList.contains('done') ? true : false
+            saveTodos()
+            // log(todoList[index])
         } else if (target.classList.contains('todo-delete')) {
             log('delete')
-            var todoDiv = target.parentElement
-            var index = indexOfElement(target)
-            log('delete index',  index)
             todoDiv.remove()
             todoList.splice(index, 1)
             saveTodos()
@@ -86,20 +104,23 @@ var bindDoneDeleteEdit = function() {
 
 var insertTodo = function(todo) {
     // 添加到 container 中
-    var todoContainer = document.querySelector('#id-div-container')
+    var todoContainer = e('#id-div-container')
     var t = templateTodo(todo)
     // 添加元素
     todoContainer.insertAdjacentHTML('beforeend', t);
 }
 
 var templateTodo = function(todo) {
+    var i = todo.finish ? 'done' : ''
     var t = `
-        <div class='todo-cell'>
+        <div class='todo-cell ${i}'>
             <button class='todo-done'>完成</button>
             <button class='todo-delete'>删除</button>
-            <button class='todo-edit'>编辑</button>
-            <span contenteditable='false'>${todo.task}</span>
-            <span>${todo.time}</span>
+            <button class='todo-edit'>更新</button>
+            <span class='todo-content'>
+                <span class='task-time' >${todo.time}</span>
+                <span contenteditable='false'>${todo.task}</span>
+            </span>
         </div>
     `
     return t
@@ -119,10 +140,10 @@ var loadTodos = function() {
 // 返回自己在父元素中的下标
 var indexOfElement = function(element) {
     var parent = element.parentElement
-    var parent = parent.parentElement
+    // log('parent', parent)
     for (var i = 0; i < parent.children.length; i++) {
         var e = parent.children[i]
-        if (e === element.parentElement) {
+        if (e === element) {
             return i
         }
     }
@@ -137,13 +158,21 @@ var toggleClass = function(element, className) {
     }
 }
 
+var zfill = function(string) {
+    var s = String(string)
+    if (s.length == 1) {
+        s = '0' + s
+    }
+    return s
+}
+
 var currentTime = function() {
     var d = new Date()
-    var month = d.getMonth() + 1
-    var date = d.getDate()
-    var hours = d.getHours()
-    var minutes = d.getMinutes()
-    var seconds = d.getSeconds()
+    var month = zfill(d.getMonth() + 1)
+    var date = zfill(d.getDate())
+    var hours = zfill(d.getHours())
+    var minutes = zfill(d.getMinutes())
+    var seconds = zfill(d.getSeconds())
     var timeString = `${month}/${date} ${hours}:${minutes}:${seconds}`
     return timeString
 }
@@ -159,9 +188,14 @@ var initBrower = function() {
 
 var bindEvents = function() {
     bindAddButton()
-    bindKeyEnter()
+
+    bindUpdateKeyEnter()
+
     bindDoneDeleteEdit()
+
     bindBlur()
+
+    bindAddkeyUp()
 }
 
 var todoList = []
